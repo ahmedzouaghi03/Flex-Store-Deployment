205fd60b822292fd3aceff2145711369348f1c11
Ahmed Zouaghi <ahmedzouaghi2003@gmail.com>
2026-07-28 00:12:47 +0100

    fix: fix imgbb images fetch issue

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -15,6 +15,7 @@ const nextConfig: NextConfig = {
       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
       { protocol: "https", hostname: "utfs.io", pathname: "/**" },
       { protocol: "https", hostname: "ufs.sh", pathname: "/**" },
+      { protocol: "https", hostname: "i.ibb.co", pathname: "/**" },
     ],
     unoptimized: process.env.NODE_ENV === "development",
   },
